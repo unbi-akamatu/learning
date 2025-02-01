f50c00f208aa8d53e1533ff730766f1313c33ab5
unbi-akamatu <unbijour.shirume@gmail.com>
2025-02-01 20:54:57 +0900

    CDNに修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone", // ✅ 追加！
+  reactStrictMode: true,
+  env: {
+    NEWT_CDN_API_TOKEN: process.env.NEWT_CDN_API_TOKEN,
+    NEWT_SPACE_UID: process.env.NEWT_SPACE_UID,
+  },
   images: {
     remotePatterns: [
       {
